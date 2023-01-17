064160a1bbbaca571afe26b4d46e37a88707179c
Kyle Butler <webundefinedcompany@gmail.com>
2023-01-17 14:23:53 -0800

    remove finished classes

--- assets/js/buyButtons.js
+++ assets/js/buyButtons.js
@@ -446,149 +446,6 @@
         });
         ShopifyBuy.UI.onReady(client).then(function (ui) {
             ui.createComponent("product", {
-                id: "7425427472559",
-                node: document.getElementById(
-                    "product-component-1671563007072"
-                ),
-                moneyFormat: "%24%7B%7Bamount%7D%7D",
-                options: {
-                    product: {
-                        styles: {
-                            product: {
-                                "@media (min-width: 601px)": {
-                                    "max-width": "calc(25% - 20px)",
-                                    "margin-left": "20px",
-                                    "margin-bottom": "50px"
-                                },
-                                "text-align": "left"
-                            },
-                            button: {
-                                ":hover": {
-                                    "background-color": "#2b92b8"
-                                },
-                                "background-color": "#30a2cc",
-                                ":focus": {
-                                    "background-color": "#2b92b8"
-                                }
-                            }
-                        },
-                        contents: {
-                            img: false,
-                            button: false,
-                            buttonWithQuantity: true,
-                            title: false,
-                            price: false
-                        },
-                        text: {
-                            button: "Add to cart"
-                        }
-                    },
-                    productSet: {
-                        styles: {
-                            products: {
-                                "@media (min-width: 601px)": {
-                                    "margin-left": "-20px"
-                                }
-                            }
-                        }
-                    },
-                    modalProduct: {
-                        contents: {
-                            img: false,
-                            imgWithCarousel: true,
-                            button: false,
-                            buttonWithQuantity: true
-                        },
-                        styles: {
-                            product: {
-                                "@media (min-width: 601px)": {
-                                    "max-width": "100%",
-                                    "margin-left": "0px",
-                                    "margin-bottom": "0px"
-                                }
-                            },
-                            button: {
-                                ":hover": {
-                                    "background-color": "#2b92b8"
-                                },
-                                "background-color": "#30a2cc",
-                                ":focus": {
-                                    "background-color": "#2b92b8"
-                                }
-                            }
-                        },
-                        text: {
-                            button: "Add to cart"
-                        }
-                    },
-                    option: {},
-                    cart: {
-                        styles: {
-                            button: {
-                                ":hover": {
-                                    "background-color": "#2b92b8"
-                                },
-                                "background-color": "#30a2cc",
-                                ":focus": {
-                                    "background-color": "#2b92b8"
-                                }
-                            }
-                        },
-                        text: {
-                            total: "Subtotal",
-                            button: "Checkout"
-                        }
-                    },
-                    toggle: {
-                        styles: {
-                            toggle: {
-                                "background-color": "#30a2cc",
-                                ":hover": {
-                                    "background-color": "#2b92b8"
-                                },
-                                ":focus": {
-                                    "background-color": "#2b92b8"
-                                }
-                            }
-                        }
-                    }
-                }
-            });
-        });
-    }
-})();
-
-// Cryo 2
-
-(function () {
-    var scriptURL =
-        "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
-    if (window.ShopifyBuy) {
-        if (window.ShopifyBuy.UI) {
-            ShopifyBuyInit();
-        } else {
-            loadScript();
-        }
-    } else {
-        loadScript();
-    }
-    function loadScript() {
-        var script = document.createElement("script");
-        script.async = true;
-        script.src = scriptURL;
-        (
-            document.getElementsByTagName("head")[0] ||
-            document.getElementsByTagName("body")[0]
-        ).appendChild(script);
-        script.onload = ShopifyBuyInit;
-    }
-    function ShopifyBuyInit() {
-        var client = ShopifyBuy.buildClient({
-            domain: "themusculartherapyinstitute.myshopify.com",
-            storefrontAccessToken: "681e6e10352c494bac00c142e1794b3c"
-        });
-        ShopifyBuy.UI.onReady(client).then(function (ui) {
-            ui.createComponent("product", {
                 id: "7425428226223",
                 node: document.getElementById(
                     "product-component-1671563387646"
@@ -701,7 +558,7 @@
     }
 })();
 
-// Cryo 3
+// Cryo 2
 
 (function () {
     var scriptURL =
@@ -844,7 +701,7 @@
     }
 })();
 
-// Cryo 4
+// Cryo 3
 
 (function () {
     var scriptURL =
@@ -987,7 +844,7 @@
     }
 })();
 
-// Cryo 5
+// Cryo 4
 
 (function () {
     var scriptURL =
