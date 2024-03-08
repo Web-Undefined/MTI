2125565c47e330d7f8aeffba054809dc2c1419f2
Kyle Butler <webundefinedcompany@gmail.com>
2024-03-08 08:46:31 -0800

    new may class

--- assets/js/buyButtons.js
+++ assets/js/buyButtons.js
@@ -1387,3 +1387,142 @@
         });
     }
 })();
+
+// May 17-18 Class
+
+(function () {
+    var scriptURL =
+        "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+    if (window.ShopifyBuy) {
+        if (window.ShopifyBuy.UI) {
+            ShopifyBuyInit();
+        } else {
+            loadScript();
+        }
+    } else {
+        loadScript();
+    }
+    function loadScript() {
+        var script = document.createElement("script");
+        script.async = true;
+        script.src = scriptURL;
+        (
+            document.getElementsByTagName("head")[0] ||
+            document.getElementsByTagName("body")[0]
+        ).appendChild(script);
+        script.onload = ShopifyBuyInit;
+    }
+    function ShopifyBuyInit() {
+        var client = ShopifyBuy.buildClient({
+            domain: "themusculartherapyinstitute.myshopify.com",
+            storefrontAccessToken: "681e6e10352c494bac00c142e1794b3c"
+        });
+        ShopifyBuy.UI.onReady(client).then(function (ui) {
+            ui.createComponent("product", {
+                id: "7754872258735",
+                node: document.getElementById(
+                    "product-component-1709916309301"
+                ),
+                moneyFormat: "%24%7B%7Bamount%7D%7D",
+                options: {
+                    product: {
+                        styles: {
+                            product: {
+                                "@media (min-width: 601px)": {
+                                    "max-width": "calc(25% - 20px)",
+                                    "margin-left": "20px",
+                                    "margin-bottom": "50px"
+                                }
+                            },
+                            button: {
+                                ":hover": {
+                                    "background-color": "#2b92b8"
+                                },
+                                "background-color": "#30a2cc",
+                                ":focus": {
+                                    "background-color": "#2b92b8"
+                                }
+                            }
+                        },
+                        contents: {
+                            button: false,
+                            buttonWithQuantity: true
+                        },
+                        text: {
+                            button: "Add to cart"
+                        }
+                    },
+                    productSet: {
+                        styles: {
+                            products: {
+                                "@media (min-width: 601px)": {
+                                    "margin-left": "-20px"
+                                }
+                            }
+                        }
+                    },
+                    modalProduct: {
+                        contents: {
+                            img: false,
+                            imgWithCarousel: true,
+                            button: false,
+                            buttonWithQuantity: true
+                        },
+                        styles: {
+                            product: {
+                                "@media (min-width: 601px)": {
+                                    "max-width": "100%",
+                                    "margin-left": "0px",
+                                    "margin-bottom": "0px"
+                                }
+                            },
+                            button: {
+                                ":hover": {
+                                    "background-color": "#2b92b8"
+                                },
+                                "background-color": "#30a2cc",
+                                ":focus": {
+                                    "background-color": "#2b92b8"
+                                }
+                            }
+                        },
+                        text: {
+                            button: "Add to cart"
+                        }
+                    },
+                    option: {},
+                    cart: {
+                        styles: {
+                            button: {
+                                ":hover": {
+                                    "background-color": "#2b92b8"
+                                },
+                                "background-color": "#30a2cc",
+                                ":focus": {
+                                    "background-color": "#2b92b8"
+                                }
+                            }
+                        },
+                        text: {
+                            total: "Subtotal",
+                            button: "Checkout"
+                        }
+                    },
+                    toggle: {
+                        styles: {
+                            toggle: {
+                                "background-color": "#30a2cc",
+                                ":hover": {
+                                    "background-color": "#2b92b8"
+                                },
+                                ":focus": {
+                                    "background-color": "#2b92b8"
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        });
+    }
+})();
